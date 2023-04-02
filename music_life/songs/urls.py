2db3e95fbd32cf19422340d88160e6430f417aa6
Sokolnikov Bogdan <sokolnikovbogdan1@gmail.com>
2023-04-02 16:16:14 +0300

    almost fix bug

--- music_life/songs/urls.py
+++ music_life/songs/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('authorslist/', AuthorsList.as_view(), name='authorslist'),
+    path('songs/', AuthorsList.as_view(), name='songs'),
     path('logout/', logout_user, name='logout'),
     path('login/', LoginUser.as_view(), name='login'),
     path('about/', AboutView.as_view(), name='about'),
--- music_life/songs/views.py
+++ music_life/songs/views.py
@@ -124,16 +124,16 @@ class SongsGener(DataMixin, ListView):
 
 class AuthorsList(DataMixin, ListView):
     model = Author
-    template_name = 'songs/authorslist.html'
-    context_object_name = 'authorslist'
+    template_name = 'songs/songs.html'
+    context_object_name = 'songs'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        authors = Author.objects.all().order_by('name')
-        context['authors'] = authors
-        g_def = self.get_user_context(title='Исполнители', is_authorslist=1)
-        context = dict(list(context.items()) + list(g_def.items()))
-        return context
+        g_def = self.get_user_context(title="Songs")
+        return dict(list(context.items()) + list(g_def.items()))
+
+    def get_queryset(self):
+        return Author.objects.all().order_by('name')
 
 
 class RegisterUser(DataMixin, CreateView):
